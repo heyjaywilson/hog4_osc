6ca493f6ce8ad27013ffe66ad3a223d80a322224
Maegan Wilson <maegan.wilson@highend.com>
2018-02-21 11:27:55 -0600

    This works...

--- web/socket-synth.js
+++ web/socket-synth.js
@@ -1,122 +1,119 @@
 var example = example || {};
 
-(function () {
-    "use strict";
+(function() {
+  "use strict";
 
-    var freqTransform = function (value) {
-        return (value * 6000) + 60;
+  var freqTransform = function(value) {
+    return value * 6000 + 60;
+  };
+
+  var identityTransform = function(value) {
+    return value;
+  };
+
+  var carrierSpec = {
+    freq: {
+      inputPath: "carrier.freq.value",
+      transform: freqTransform
+    },
+    mul: {
+      inputPath: "carrier.mul",
+      transform: identityTransform
+    }
+  };
+
+  var modulatorSpec = {
+    freq: {
+      inputPath: "modulator.freq.value",
+      transform: freqTransform
+    },
+    mul: {
+      inputPath: "modulator.mul",
+      transform: freqTransform
+    }
+  };
+
+  example.SocketSynth = function() {
+    this.oscPort = new osc.WebSocketPort({
+      url: "ws://localhost:8080"
+    });
+
+    this.listen();
+    this.oscPort.open();
+
+    this.oscPort.socket.onmessage = function(e) {
+      console.log("message", e);
     };
 
-    var identityTransform = function (value) {
-        return value;
+    this.valueMap = {
+      "/knobs/0": carrierSpec.freq,
+      "/fader1/out": carrierSpec.freq,
+
+      "/knobs/1": carrierSpec.mul,
+      "/fader2/out": carrierSpec.mul,
+
+      "/knobs/2": modulatorSpec.freq,
+      "/fader3/out": modulatorSpec.freq,
+
+      "/knobs/3": modulatorSpec.mul,
+      "/fader4/out": modulatorSpec.mul
     };
 
-    var carrierSpec = {
+    this.synth = flock.synth({
+      synthDef: {
+        id: "carrier",
+        ugen: "flock.ugen.sin",
         freq: {
-            inputPath: "carrier.freq.value",
-            transform: freqTransform
+          ugen: "flock.ugen.value",
+          rate: "audio",
+          value: 400,
+          add: {
+            id: "modulator",
+            ugen: "flock.ugen.sin",
+            freq: {
+              ugen: "flock.ugen.value",
+              rate: "audio",
+              value: 124
+            },
+            mul: 100
+          }
         },
-        mul: {
-            inputPath: "carrier.mul",
-            transform: identityTransform
-        }
-    };
+        mul: 0.3
+      }
+    });
+  };
 
-    var modulatorSpec = {
-        freq: {
-            inputPath: "modulator.freq.value",
-            transform: freqTransform
-        },
-        mul: {
-            inputPath: "modulator.mul",
-            transform: freqTransform
-        }
-    };
+  example.SocketSynth.prototype.listen = function() {
+    this.oscPort.on("open", this.play.bind(this));
+    this.oscPort.on("message", this.mapMessage.bind(this));
+    this.oscPort.on("message", function(msg) {
+      console.log("message", msg);
+    });
+    this.oscPort.on("close", this.pause.bind(this));
+  };
 
-    example.SocketSynth = function () {
-        this.oscPort = new osc.WebSocketPort({
-            url: "ws://localhost:8081"
-        });
+  example.SocketSynth.prototype.play = function() {
+    if (!flock.enviro.shared) {
+      flock.init();
+    }
 
-        this.listen();
-        this.oscPort.open();
+    this.synth.play();
+  };
 
-        this.oscPort.socket.onmessage = function (e) {
-            console.log("message", e);
-        };
-        
-        this.valueMap = {
-            "/knobs/0": carrierSpec.freq,
-            "/fader1/out": carrierSpec.freq,
+  example.SocketSynth.prototype.pause = function() {
+    this.synth.pause();
+  };
 
-            "/knobs/1": carrierSpec.mul,
-            "/fader2/out": carrierSpec.mul,
+  example.SocketSynth.prototype.mapMessage = function(oscMessage) {
+    $("#message").text(fluid.prettyPrintJSON(oscMessage));
 
-            "/knobs/2": modulatorSpec.freq,
-            "/fader3/out": modulatorSpec.freq,
+    var address = oscMessage.address;
+    var value = oscMessage.args[0];
+    var transformSpec = this.valueMap[address];
 
-            "/knobs/3": modulatorSpec.mul,
-            "/fader4/out": modulatorSpec.mul
-        };
-
-        this.synth = flock.synth({
-            synthDef: {
-                id: "carrier",
-                ugen: "flock.ugen.sin",
-                freq: {
-                    ugen: "flock.ugen.value",
-                    rate: "audio",
-                    value: 400,
-                    add: {
-                        id: "modulator",
-                        ugen: "flock.ugen.sin",
-                        freq: {
-                            ugen: "flock.ugen.value",
-                            rate: "audio",
-                            value: 124
-                        },
-                        mul: 100
-                    }
-                },
-                mul: 0.3
-            }
-        });
-
-
-    };
-
-    example.SocketSynth.prototype.listen = function () {
-        this.oscPort.on("open", this.play.bind(this));
-        this.oscPort.on("message", this.mapMessage.bind(this));
-        this.oscPort.on("message", function (msg) {
-            console.log("message", msg);
-        });
-        this.oscPort.on("close", this.pause.bind(this));
-    };
-
-    example.SocketSynth.prototype.play = function () {
-        if (!flock.enviro.shared) {
-            flock.init();
-        }
-
-        this.synth.play();
-    };
-
-    example.SocketSynth.prototype.pause = function () {
-        this.synth.pause();
-    };
-
-    example.SocketSynth.prototype.mapMessage = function (oscMessage) {
-        $("#message").text(fluid.prettyPrintJSON(oscMessage));
-
-        var address = oscMessage.address;
-        var value = oscMessage.args[0];
-        var transformSpec = this.valueMap[address];
-
-        if (transformSpec) {
-            var transformed = transformSpec.transform(value);
-            this.synth.set(transformSpec.inputPath, transformed);
-        }
-    };
-
-}());
+    if (transformSpec) {
+      var transformed = transformSpec.transform(value);
+      this.synth.set(transformSpec.inputPath, transformed);
+    }
+  };
+})();
