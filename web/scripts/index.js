0f8e91d33006b487a7f3733d1034f13f93958be0
Maegan Wilson <maegan@maeganwilson.com>
2018-02-26 18:18:08 -0500

    adding more methods for sorting out button pushes
    Signed-off-by: Maegan Wilson <maegan@maeganwilson.com>

--- web/scripts/index.js
+++ web/scripts/index.js
@@ -51,8 +51,82 @@ var getText = function(id) {
   return Array.from(text);
 };
 
+var getButtonPushes = function(arr) {
+  arr.forEach(function(bttn) {
+    switch (bttn) {
+      case "1":
+        console.log("button pushed 1");
+        sendButtonPush("one");
+        break;
+      case "2":
+        console.log("button pushed 2");
+        sendButtonPush("two");
+        break;
+      case "3":
+        console.log("button pushed 3");
+        break;
+      case "4":
+        console.log("button pushed 4");
+        break;
+      case "5":
+        console.log("button pushed 5");
+        break;
+      case "6":
+        console.log("button pushed 6");
+        break;
+      case "7":
+        console.log("button pushed 7");
+        break;
+      case "8":
+        console.log("button pushed 8");
+        break;
+      case "9":
+        console.log("button pushed 9");
+        break;
+      case "0":
+        console.log("button pushed 0");
+        break;
+      case "+":
+        console.log("button pushed +");
+        break;
+      case "-":
+        console.log("button pushed -");
+        break;
+      case ">":
+        console.log("button pushed thru");
+        break;
+      default:
+        alert(bttn + " is not a valid Hog hardware button");
+    }
+  });
+};
+
+var sendButtonPush = function(button) {
+  var message = "/hog/hardware/" + button;
+  port.send({
+    address: message,
+    args: [
+      {
+        type: "f",
+        value: 0
+      }
+    ]
+  });
+  port.send({
+    address: message,
+    args: [
+      {
+        type: "f",
+        value: 1
+      }
+    ]
+  });
+  console.log("sending message: " + message);
+};
+
 var highlight = function() {
   let button_pushes = getText("fixtureSelect");
+  getButtonPushes(button_pushes);
   console.log(button_pushes);
   let mess = "/hog/hardware/highlight";
 };
